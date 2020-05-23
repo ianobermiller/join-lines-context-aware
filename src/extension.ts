37226a5d14f61fe7e61e22b72f544943b73809db
Ian Obermiller <ian@obermillers.com>
2020-05-23 06:06:47 -0700

    Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,16 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('join-lines-context-aware.join', () => {
-		vscode.window.showInformationMessage('Hello World from join-lines-context-aware!');
-	});
+  const disposable = vscode.commands.registerCommand(
+    'join-lines-context-aware.join',
+    () => {
+      vscode.window.showInformationMessage(
+        'Hello World from join-lines-context-aware!',
+      );
+    },
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
