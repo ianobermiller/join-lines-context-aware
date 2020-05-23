fa1cc649601f26471bf0f67019ef3ec04f4ee3ba
Ian Obermiller <ian@obermillers.com>
2020-05-23 06:34:40 -0700

    Initial implementation
    Ported from https://github.com/ianobermiller/join-lines-smarter

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,45 @@
 import * as vscode from 'vscode';
+import joinLines from './joinLines';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'join-lines-context-aware.join',
-    () => {
-      vscode.window.showInformationMessage(
-        'Hello World from join-lines-context-aware!',
-      );
-    },
+    joinLinesCommand,
   );
 
   context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
+
+async function joinLinesCommand() {
+  const textEditor = vscode.window.activeTextEditor;
+  if (!textEditor) {
+    return;
+  }
+
+  const {document} = textEditor;
+
+  await textEditor.edit(editBuilder => {
+    for (const selection of textEditor.selections) {
+      const isLastLine = selection.end.line === document.lineCount - 1;
+      if (isLastLine) {
+        return;
+      }
+
+      const firstLine = document.lineAt(selection.start.line).text;
+      const secondLine = document.lineAt(selection.start.line + 1).text;
+
+      const newLines = joinLines(firstLine, secondLine);
+      editBuilder.replace(
+        new vscode.Range(
+          selection.start.line,
+          0,
+          selection.start.line + 1,
+          secondLine.length,
+        ),
+        newLines,
+      );
+    }
+  });
+}
---
+++ src/joinLines.ts
@@ -0,0 +1,60 @@
+const joinMarker = 'nDW;C%yh3PK}2Lq8ne-2$=pvjSF+Th';
+const stringConcatRegex = new RegExp(
+  "(' ?(\\+|\\.)?" + joinMarker + '\')|(" ?(\\+|\\.)?' + joinMarker + '")',
+);
+
+export default function joinLines(
+  firstLine: string,
+  secondLine: string,
+): string {
+  let newLines = firstLine.trimRight() + joinMarker + secondLine.trimLeft();
+  newLines = newLines.replace(stringConcatRegex, '');
+
+  if (firstLine.trim().startsWith('//')) {
+    newLines = newLines.replace(joinMarker + '// ', ' ');
+  }
+
+  if (firstLine.trim().startsWith('* ')) {
+    newLines = newLines.replace(joinMarker + '* ', ' ');
+  }
+
+  if (firstLine.trim().startsWith('# ')) {
+    newLines = newLines.replace(joinMarker + '# ', ' ');
+  }
+
+  if (firstLine.trim().startsWith('; ')) {
+    newLines = newLines.replace(joinMarker + '; ', ' ');
+  }
+
+  // Eliminate trailing comma and space when joining a line with a comma
+  // with a line with a closing bracket
+  if (firstLine.trim().endsWith(',') && startsWithClosingBracket(secondLine)) {
+    newLines = newLines.replace(',' + joinMarker, '');
+  }
+
+  const shouldSpace =
+    !endsWithOpeningBracket(firstLine) && !startsWithClosingBracket(secondLine);
+  newLines = newLines.replace(joinMarker, shouldSpace ? ' ' : '');
+  return newLines;
+}
+
+function endsWithOpeningBracket(s: string): boolean {
+  s = s.trim();
+  const v = s[s.length - 1];
+  if (v === '{' || v === '[' || v === '(') {
+    return true;
+  }
+
+  // Preserve the space if it looks like `<` is an operator (with a space on
+  // the left-hand side)
+  if (v === '<' && s[s.length - 2] !== ' ') {
+    return true;
+  }
+
+  return false;
+}
+
+function startsWithClosingBracket(s: string): boolean {
+  const v = s.trim()[0];
+  return v === '}' || v === ']' || v === ')' || v === '>';
+}
